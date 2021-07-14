807f244b4354f18c0a4124a0def527d885eb53a5
Marco Pereira <marcopereira@gmx.ch>
2021-07-14 14:42:33 +0200

    utility functions can be written in JS to facilitate the writting of functions

--- src/routes/expression-utils.js
+++ src/routes/expression-utils.js
@@ -10,26 +10,14 @@
  * @param {string} condition - Set that is parent of the possible answers  
  * @returns {Object}
  */
-export function convertMultipleChoice(chosenAnswers, condition){
-    conditionsDict = {
-        s_cough:['s_difficult_breathing', 's_coryza', 's_cough', 's_none'],
-        s_danger_look:['s_convulsion','s_lethargic','s_none'],
-        s_danger_ask:['s_no_drink', 's_throw','s_convulsion_epi','s_none'],
-        s_immunizations_received:['s_bcg','s_opv0','s_opv1','s_opv2','s_opv3','s_pentavalent1','s_pentavalent2','s_pentavalent3','s_ipv1','s_ipv2','s_measles1','s_measles2','s_none'],
-        s_malnutrition:['s_wasting_severe','s_oedema_bilateral','s_no_weight_gain','s_not_sucking','s_none']
-    }
-    chosenAnswersList = chosenAnswers.split(" ")
-    // Assumption 1: One option will not come in multiple lists
-    // Assumption 2: The key of the multiple choice is allways the
-    // same depending on what multiple choice question was answered
-    // e.g. the key to the cough multiple choice answers will always
-    // be s_cough ("s_cough":"s_difficult_breathing" ...)
-    individualSymptoms = {}
+export function convertMultipleChoice(chosenAnswers, condition, conditionsDict){
+    let chosenAnswersList = chosenAnswers.split(" ")
+    var individualSymptoms = {}
     if (chosenAnswers === "s_none"){
             individualSymptoms["s_none"] = true
         return individualSymptoms
     }else{
-        tmp = conditionsDict[condition];
+        let tmp = conditionsDict[condition];
         tmp.forEach(function(element){
             if (chosenAnswersList.indexOf(element) != -1){
                 individualSymptoms[element] = true
@@ -46,4 +34,13 @@ export function converter(x) {
     }else{
         return x;
     }
+}
+
+export function indexOf(collection, target) {
+    for(var val=0; val<collection.length; val++){
+       if(collection[val] === target){
+          return val;
+        }
+    }
+    return -1;
 }
