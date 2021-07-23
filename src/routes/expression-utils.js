b25474633f3d86920ab66c48c49f30d605c5dc7d
Marco Pereira <marcopereira@gmx.ch>
2021-07-23 12:51:41 +0200

    added more simple function to check wether substring is part of a string; added parameter for delimiter

--- src/routes/expression-utils.js
+++ src/routes/expression-utils.js
@@ -10,36 +10,42 @@
  * @param {string} condition - Set that is parent of the possible answers  
  * @returns {Object}
  */
-export function convertMultipleChoice(chosenAnswers, condition, conditionsDict){
-    let chosenAnswersList = chosenAnswers.split(" ")
+export function convertMultipleChoice(chosenAnswers, condition, conditionsDict, delimiter = " ") {
+    // multiples spaces scenario (works)
+    let chosenAnswersList = chosenAnswers.split(delimiter)
     var individualSymptoms = {}
-    if (chosenAnswers === "s_none"){
-            individualSymptoms["s_none"] = true
+    if (chosenAnswers === "s_none") {
+        individualSymptoms["s_none"] = true
         return individualSymptoms
-    }else{
+    } else {
         let tmp = conditionsDict[condition];
-        tmp.forEach(function(element){
-            if (chosenAnswersList.indexOf(element) != -1){
+        tmp.forEach(function (element) {
+            if (chosenAnswersList.indexOf(element) != -1) {
                 individualSymptoms[element] = true
-            }else individualSymptoms[element] = false
+            } else individualSymptoms[element] = false
         })
-    }return individualSymptoms;
+    } return individualSymptoms;
 }
 
-export function converter(x) {
-    if(x === 1 || x === '1'){
+export function checkAnswers(chosenAnswers, target, delimiter = " ") {
+    let chosenAnswersList = chosenAnswers.split(delimiter)
+    return indexOf(chosenAnswersList, target) != -1
+}
+
+export function convertToBoolean(x) {
+    if (x === 1 || x === '1' || x === 'true' || x === true) {
         return true;
-    }else if(x === 0 || x === '0' || x === -1 || x === '-1'){
+    } else if (x === 0 || x === '0' || x === -1 || x === '-1' || x === '' || x === 'false' || x === false) {
         return false;
-    }else{
-        return x;
+    } else {
+        return true;
     }
 }
 
 export function indexOf(collection, target) {
-    for(var val=0; val<collection.length; val++){
-       if(collection[val] === target){
-          return val;
+    for (var val = 0; val < collection.length; val++) {
+        if (collection[val] === target) {
+            return val;
         }
     }
     return -1;
