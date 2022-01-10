f5440b0cb3869edfe9a02c87fd5cc7717d5f310b
magp18 <marcopereira@gmx.ch>
2022-01-10 16:07:16 +0000

    as is

---
+++ install.js
@@ -0,0 +1,27 @@
+var fs = require('fs')
+var resolve = require('path').resolve
+var join = require('path').join
+var cp = require('child_process')
+var os = require('os')
+
+// get library path
+var lib = resolve(__dirname, './languages/')
+
+fs.readdirSync(lib)
+  .forEach(function (mod) {
+    var modPath = join(lib, mod)
+// ensure path has package.json
+if (!fs.existsSync(join(modPath, 'package.json'))) return
+
+// npm binary based on OS
+var npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm'
+
+// install folder
+cp.spawn(npmCmd, ['i'], { env: process.env, cwd: modPath, stdio: 'inherit' })
+
+//build	
+//cp.spawn('make',[], { env: process.env, cwd: modPath, stdio: 'inherit' })
+
+
+
+  })
